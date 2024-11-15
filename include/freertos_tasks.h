a54f4999ff1e3b3e636c47a90ed8904133d9ee59
Facundo <facundo.guerrero.pozzi@mi.unc.edu.ar>
2024-11-15 14:28:39 -0300

    documentation added

--- include/freertos_tasks.h
+++ include/freertos_tasks.h
@@ -3,6 +3,13 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
+/**
+ * @brief second in ms
+ */
 #define SEC 1000
 
+/**
+ * @brief UartTask for freeRTOS
+ * it sends data for uart every specefied time
+ */
 void vSend_UART_task(void* args);
--- include/uart.h
+++ include/uart.h
@@ -4,12 +4,34 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/usart.h>
 
+/**
+ * @brief stm32 port using for uart configuration
+ */
 #define UART_PORT GPIOA
+/**
+ * @brief pin used for TX in uart1
+ */
 #define UART_TX_PIN GPIO9
+/**
+ * @brief pin used for RX in uart1
+ */
 #define UART_RX_PIN GPIO10
+/**
+ * @brief uart baurate configuration
+ */
 #define UART_BAUDRATE 9600
+/**
+ * @brief uart databits
+ */
 #define UART_DATABITS 8
 
+/**
+ * @brief variable to transmit, it maybe change in the future
+ */
 extern char dato;
 
+/**
+ * @brief setup usart configurations
+ * gpio clock and uart pheripherial configuration for uart transmitions
+ */
 void configure_usart(void);
